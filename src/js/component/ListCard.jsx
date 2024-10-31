3779822038a50b2b7a4656f7b0b67f6c1a8f05df
Felipe Coste <32721311+felipecoste@users.noreply.github.com>
2024-10-31 22:30:02 +0000

    se agregan estilos

--- src/js/component/ListCard.jsx
+++ src/js/component/ListCard.jsx
@@ -1,11 +1,67 @@
-import React from "react";
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+function App() {
+  const [tasksList, setTasksList] = useState([]);
+  const [inputValue, setInputValue] = useState('');
 
 
-const ListCard = () => {
+  const addNewTask = (e) => {
+    if (e.key === 'Enter' && inputValue.trim()) {
+      const newTask = {
+        id: uuidv4(),
+        task: inputValue  // usar inputValueen vez de e.target.value
+      };
+      setTasksList([...tasksList, newTask]);  // agregar nueva tarea
+      setInputValue('');  // limpiar input
+    }
+  };
 
-    return (
-        <h1>a</h1>
-    );
-};
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);  //actualizar input value
+  };
 
-export default ListCard;
+  const deleteTask = (id) => {
+    setTasksList(tasksList.filter((task) => task.id !== id)); // borrar task por id
+  };
+
+  return (
+    <div className="container d-flex justify-content-center mt-5">
+      <div className="card" style={{ width: '25rem' }}>
+        <div className="card-body">
+
+          <div className="container d-flex justify-content-center">
+            <h1 className='card-title my-5'>To Do List</h1>
+          </div>
+
+          <div className="container d-flex justify-content-center mb-3">
+            <input
+              type="text"
+              placeholder="Escribe una tarea y presiona Enter"
+              value={inputValue}
+              onChange={handleInputChange}
+              onKeyDown={addNewTask}
+            />
+          </div>
+
+          <div className="container d-flex justify-content-center">
+            <ul className='list-group'>
+              {tasksList.map((task) => (
+                <li
+                  className='d-flex justify-content-between list-group-item p-3 '
+                  key={task.id}
+                >
+                  {task.task}
+                  <button onClick={() => deleteTask(task.id)}>Eliminar</button>
+                </li>
+              ))}
+
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
